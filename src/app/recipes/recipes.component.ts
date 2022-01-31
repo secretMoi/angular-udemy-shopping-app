cce55eeeac1599c282de533ab2cc03993e00475a
quentin.bolsee <quentin.bolsee@lampiris.be>
2022-01-31 12:35:31 +0100

    Improving the Reactive Service with Observables (Subjects)

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,6 +1,5 @@
 import {Component, Injectable, OnInit} from '@angular/core';
 import {Recipe} from "./recipe.model";
-import {RecipeService} from "./recipe.service";
 
 @Component({
   selector: 'app-recipes',
@@ -9,16 +8,11 @@ import {RecipeService} from "./recipe.service";
 })
 @Injectable()
 export class RecipesComponent implements OnInit {
-  selectedRecipe!: Recipe;
 
-  constructor(private recipeService: RecipeService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.recipeService.recipeSelected.subscribe(
-      (recipe: Recipe) => {
-        this.selectedRecipe = recipe;
-      }
-    );
+
   }
 
 }
