89ebbac20ae3f41172bff823b9330f5ebbb35581
quentin.bolsee <quentin.bolsee@lampiris.be>
2022-02-04 10:08:01 +0100

    Updating existing Items

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -14,7 +14,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
 
   subscription!: Subscription;
   editMode: boolean = false;
-  editedItemIndex?: number;
+  editedItemIndex!: number;
   editedItem?: Ingredient;
 
   constructor(private shoppingListService :ShoppingListService) { }
@@ -36,7 +36,13 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     onAddItem(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
-    this.shoppingListService.addIngredient(newIngredient);
+
+    if(this.editMode) {
+      this.shoppingListService.updateIngredient(this.editedItemIndex, newIngredient);
+    }
+    else {
+      this.shoppingListService.addIngredient(newIngredient);
+    }
   }
 
   ngOnDestroy() {
