8e3d3a9d975a534129d52e9e655570052255b37e
secretMoi <win.moi@hotmail.com>
2022-02-04 11:18:30 +0100

    Submitting the Recipe Edit Form

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -19,6 +19,10 @@ export class RecipesListComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.recipeService.recipesChanged.subscribe(
+      (recipes: Recipe[]) => this.recipes = recipes
+    );
+
     this.recipes = this.recipeService.getRecipes();
   }
 
