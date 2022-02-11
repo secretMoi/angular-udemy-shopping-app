93e7617aba4cfed2ba300d10bdf1400ddd18295c
secretMoi <win.moi@hotmail.com>
2022-02-11 11:03:03 +0100

    Storing recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,11 +1,27 @@
 import {Injectable} from "@angular/core";
-import {HttpClientModule} from "@angular/common/http";
+import {HttpClient} from "@angular/common/http";
+import {RecipeService} from "../recipes/recipe.service";
 
 @Injectable({
   providedIn: 'root' // permet de ne pas l'ajouter dans le appmodule.ts
 })
 export class DataStorageService {
-  constructor(private httpClientModule: HttpClientModule) {
+  constructor(
+    private httpClientModule: HttpClient,
+    private recipeService: RecipeService
+  ) {
 
+  }
+
+  public storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.httpClientModule.put(
+      'https://angular-recipe-book-tuto-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
+      recipes
+    ).subscribe(
+      response => {
+        console.log(response);
+      }
+    )
   }
 }
