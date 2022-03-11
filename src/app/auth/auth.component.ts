3417536bbf092979894468a89bf41b6dd8694770
quentin.bolsee <quentin.bolsee@lampiris.be>
2022-03-11 11:32:15 +0100

    Creating a Component Programmatically

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,9 +1,10 @@
-import {Component, ComponentFactoryResolver} from '@angular/core';
+import {Component, ComponentFactoryResolver, ViewChild} from '@angular/core';
 import {NgForm} from "@angular/forms";
 import {AuthResponseData, AuthService} from "./auth.service";
 import {Observable} from "rxjs";
 import {Router} from "@angular/router";
 import {AlertComponent} from "../shared/alert/alert.component";
+import {PlaceholderDirective} from "../shared/placeholder/placeholder.directive";
 
 @Component({
   selector: 'app-auth',
@@ -14,6 +15,7 @@ export class AuthComponent  {
   isLoginMode: boolean = true;
   isLoading: boolean = false;
   errorMessage: string | null = null;
+  @ViewChild(PlaceholderDirective, { static: false }) alertHosts?: PlaceholderDirective;
 
   constructor(
     private authService: AuthService,
@@ -64,7 +66,10 @@ export class AuthComponent  {
   private showErrorAlert(message: string) {
     const alertComponentFactory = this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
 
+    const hostViewContainerReference = this.alertHosts?.viewContainerRef;
+    hostViewContainerReference?.clear()
 
+    hostViewContainerReference?.createComponent(alertComponentFactory);
   }
 
   onHandleError() {
