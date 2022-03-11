bea4acc9e0cdcb2e73f4f59c894f333fe3fd3a4a
secretMoi <win.moi@hotmail.com>
2022-03-11 11:25:46 +0100

    Preparing Programmatic Creation

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, ComponentFactoryResolver} from '@angular/core';
 import {NgForm} from "@angular/forms";
 import {AuthResponseData, AuthService} from "./auth.service";
 import {Observable} from "rxjs";
 import {Router} from "@angular/router";
+import {AlertComponent} from "../shared/alert/alert.component";
 
 @Component({
   selector: 'app-auth',
@@ -17,6 +18,7 @@ export class AuthComponent  {
   constructor(
     private authService: AuthService,
     private router: Router,
+    private componentFactoryResolver: ComponentFactoryResolver,
   ) { }
 
   onSwitchMode() {
@@ -50,12 +52,19 @@ export class AuthComponent  {
       errorMessage => {
         console.log(errorMessage);
         this.errorMessage = errorMessage;
+        this.showErrorAlert(errorMessage);
 
         this.isLoading = false;
       }
     );
 
     authForm.reset();
+  }
+
+  private showErrorAlert(message: string) {
+    const alertComponentFactory = this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
+
+
   }
 
   onHandleError() {
