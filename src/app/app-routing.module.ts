7781c1fc29f213ea76fa2a5897ee5d9b607be9c4
quentin.bolsee <quentin.bolsee@lampiris.be>
2022-03-14 17:06:47 +0100

    Implementing Lazy Loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,13 @@ import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 
 const appRoutes: Routes = [
-    { path: '', redirectTo: '/recipes', pathMatch: 'full' }
-  ];
+  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+  {
+    path: 'recipes',
+    // import le fichier then le nom de la classe
+    loadChildren: () => import('./recipes/recipes.module').then(x => x.RecipesModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes)],
