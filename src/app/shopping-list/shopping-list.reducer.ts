0cc645b15ea23bec4fd7064557692f3b0d6adeb7
quentin.bolsee <quentin.bolsee@lampiris.be>
2022-03-18 12:25:00 +0100

    Adding Logic to the Reducer

--- src/app/shopping-list/shopping-list.reducer.ts
+++ src/app/shopping-list/shopping-list.reducer.ts
@@ -1,4 +1,5 @@
 import {Ingredient} from "../shared/ingredient.model";
+import {Action} from "@ngrx/store";
 
 const initialState = {
   ingredients: [
@@ -7,7 +8,16 @@ const initialState = {
   ]
 };
 
-export function shoppingListReducer(state = initialState, action: any) {
+export function shoppingListReducer(state = initialState, action: Action) {
+  switch (action.type) {
+    case 'ADD_INGREDIENT':
+      // state.ingredients.push() interdit !!!
 
+      return {
+        ...state, // fais une copie des données (state)
+        ingredients: [...state.ingredients, action] // recopie les ingrédients + ajoute le nouveau
+      };
+      break;
+  }
 }
 
