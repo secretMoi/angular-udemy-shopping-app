17a9dcdcff2c077d47fb89a628ef9c2b6230cee3
secretMoi <win.moi@hotmail.com>
2022-03-18 11:01:49 +0100

    Loading Services Differently

--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -4,6 +4,7 @@ import {ShoppingEditComponent} from "./shopping-edit/shopping-edit.component";
 import {RouterModule} from "@angular/router";
 import {FormsModule} from "@angular/forms";
 import {SharedModule} from "../shared/shared.module";
+import {LoggingService} from "../logging.service";
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import {SharedModule} from "../shared/shared.module";
       { path: '', component: ShoppingListComponent },
     ]),
     SharedModule
-  ]
+  ],
+  providers: [LoggingService]
 })
 export class ShoppingListModule {
 
